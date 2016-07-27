600e53a8e47e64446916be62a22212389526aec9
Antonio José Muñoz Carmona <amcarmona@telefonica.net>
2016-07-27 12:48:44 +0200

    Despliegue en Heroku

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', $config().views.engine);
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -61,8 +61,9 @@ app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(favicon(__dirname + '/favicon.ico'));
+//app.use(favicon(__dirname + '/favicon.ico'));
 
+//Rutas
 app.use('/', routes);
 app.use('/users', users);
 
